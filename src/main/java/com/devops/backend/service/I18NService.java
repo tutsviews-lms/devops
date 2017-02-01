3aa68b017a53060bd3f5d184c47039a1420e93b2
AladinZaier <zaier.alaeddine@gmail.com>
2017-02-01 12:39:12 +0100

    logback dependency and logback configuration xml added to the project

--- src/main/java/com/devops/backend/service/I18NService.java
+++ src/main/java/com/devops/backend/service/I18NService.java
@@ -1,5 +1,7 @@
 package com.devops.backend.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -12,11 +14,15 @@ import java.util.Locale;
  */
 @Service
 public class I18NService {
+    
+    /** The application logger */
+    private static final Logger LOG = LoggerFactory.getLogger(I18NService.class);
 
     @Autowired
     private MessageSource messageSource;
 
     public String getMessage(String messageId){
+        LOG.info("Returning i18n text for messageId {}.",messageId);
         Locale local = LocaleContextHolder.getLocale();
         return getMessage(messageId,local);
     }
