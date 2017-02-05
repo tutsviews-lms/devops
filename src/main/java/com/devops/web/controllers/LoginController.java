875a9aeccf8898a07160f1063fd237fcc3c25b23
AladinZaier <zaier.alaeddine@gmail.com>
2017-02-05 22:24:15 +0100

    added Log In to the navbar

---
+++ src/main/java/com/devops/web/controllers/LoginController.java
@@ -0,0 +1,19 @@
+package com.devops.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by ALadin Zaier PC IBS on 05/02/2017.
+ */
+
+@Controller
+public class LoginController {
+
+    private static final String LOGIN_VIEW_NAME = "user/login";
+
+    @GetMapping("")
+    public String getLogin(){
+        return LOGIN_VIEW_NAME;
+    }
+}
