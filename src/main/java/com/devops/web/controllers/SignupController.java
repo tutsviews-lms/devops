ebe529b379b719d14b5b7791e66cc52710cd3183
AladinZaier <zaier.alaeddine@gmail.com>
2017-02-23 17:01:41 +0100

    added signup form for basic and pro accounts

--- src/main/java/com/devops/web/controllers/SignupController.java
+++ src/main/java/com/devops/web/controllers/SignupController.java
@@ -1,10 +1,12 @@
 package com.devops.web.controllers;
 
 import com.devops.enums.PlanEnum;
+import com.devops.web.domain.frontend.ProAccountPayload;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -22,11 +24,14 @@ public class SignupController {
     private static final String SIGNUP_PAYLOAD_MODEL_KEY = "payload";
 
     @GetMapping(SIGNUP_URL_MAPPING)
-    public String getSignupPage(@RequestParam("planId") int planId){
+    public String getSignupPage(@RequestParam("planId") int planId, Model model){
 
         if (planId != PlanEnum.BASIC.getId() && planId != PlanEnum.PRO.getId()) {
             throw new IllegalArgumentException();
         }
+
+        model.addAttribute(SIGNUP_PAYLOAD_MODEL_KEY,new ProAccountPayload());
+        model.addAttribute("registred","false");
 
         return SIGNUP_VIEW_NAME;
     }
